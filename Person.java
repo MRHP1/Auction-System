1ceb375243d31c8d22e41d2a165ac14082927283
MRHP1 <148250477+MRHP1@users.noreply.github.com>
2024-10-02 18:16:39 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person
+{
+    // The name of this user.
+    private final String name;
+
+    public Person(String name)
+    {
+        this.name = name;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+}
